3c4366404cc9986775757088c10fc2f15603711f
zubinengg <zubin.engg@gmail.com>
2025-12-28 17:13:07 +0530

    ai formatting

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,18 +1,17 @@
 extern crate rand;
 
-use std::io;
 use rand::Rng;
-
+use std::io;
 
 fn main() {
     let secret_number = rand::thread_rng().gen_range(1, 101);
     println!("The secret number is: {}", secret_number);
 
-    
     println!("Guess the number!");
     println!("Please input your guess.");
     let mut guess = String::new();
-    io::stdin().read_line(&mut guess)
+    io::stdin()
+        .read_line(&mut guess)
         .expect("Failed to read line");
     //println!("You guessed: {}", guess);
     println!("Hello, world!");
